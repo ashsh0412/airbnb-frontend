26d951128607d304047f879400b2aa73e5d39036
ashsh0412 <ashsh0412@gmail.com>
2024-09-10 13:40:47 -0400

    making header

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,9 +1,62 @@
+import {
+  Box,
+  Button,
+  HStack,
+  IconButton,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+  VStack,
+} from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
+import { FaAirbnb, FaMoon } from "react-icons/fa";
 
-export default function root() {
+export default function Root() {
+  const { isOpen, onClose, onOpen } = useDisclosure();
   return (
-    <h1>
-      I am root <Outlet />
-    </h1>
+    <Box>
+      <HStack
+        justifyContent={"space-between"}
+        py={"5"}
+        px={"10"}
+        borderBottomWidth={1}
+      >
+        <Box color="red.500">
+          <FaAirbnb size={"48"} />
+        </Box>
+        <HStack spacing={2}>
+          <IconButton
+            variant={"ghost"}
+            aria-label="Toggle dark mode"
+            icon={<FaMoon />}
+          />
+          <Button onClick={onOpen}>Log in </Button>
+          <Button>Sign up</Button>
+        </HStack>
+        <Modal onClose={onClose} isOpen={isOpen}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Log in</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <VStack>
+                <Input variant={"filled"} placeholder="Username" />
+                <Input variant={"filled"} placeholder="Password" />
+              </VStack>
+              <Button mt={4} colorScheme="red" w={"100%"}>
+                Log in
+              </Button>
+            </ModalBody>
+          </ModalContent>
+        </Modal>
+      </HStack>
+      <Outlet />
+    </Box>
   );
 }
