c27ba530115e408176226ae9602fd1d4d0f37d97
Sungho Baek <ashsh0412@gmail.com>
2024-09-20 13:13:16 -0400

    github login

--- src/components/socialLogin.tsx
+++ src/components/socialLogin.tsx
@@ -17,7 +17,12 @@ export default function SocialLogin() {
         <Divider />
       </HStack>
       <VStack>
-        <Button w={"100%"} leftIcon={<FaGithub />}>
+        <Button
+          as="a"
+          href="https://github.com/login/oauth/authorize?client_id=Ov23livcWYwt5ARgUduv&scope=read:user,user:email"
+          w={"100%"}
+          leftIcon={<FaGithub />}
+        >
           Contiue with Github
         </Button>
         <Button w={"100%"} leftIcon={<FaComment />} colorScheme="yellow">
